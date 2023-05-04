504f1cf9b80382686cb77c8244dca9532c443d88
Altior1 <43808598+Altior1@users.noreply.github.com>
2023-05-04 16:16:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Titre de la page</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="Description de la page">
+    <meta name="keywords" content="Mots-clés pour les moteurs de recherche">
+    <meta name="author" content="Nom de l'auteur">
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <header>
+      <h1>Titre principal de la page</h1>
+      <nav>
+        <ul>
+          <li><a href="#">Accueil</a></li>
+          <li><a href="#">À propos</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <section>
+        <h2>Titre de la section</h2>
+        <p>Contenu de la section</p>
+      </section>
+      <section>
+        <h2>Titre de la section</h2>
+        <p>Contenu de la section</p>
+      </section>
+    </main>
+    <footer>
+      <p>Copyright © Année | Nom de l'auteur
+    </footer>
+  </body>
+</html>
 
